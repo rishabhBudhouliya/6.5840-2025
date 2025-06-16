59369840ea308b6ba14eebad4d3d7dbbba84f55f
rishabhBudhouliya <rb5727@nyu.edu>
2025-06-16 13:36:51 -0400

    feat: add a distributed lock over client layer: clerk using kv server

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,10 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"fmt"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -9,7 +12,9 @@ type Lock struct {
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck kvtest.IKVClerk
+	ck  kvtest.IKVClerk
+	id  string
+	key string
 	// You may add code here
 }
 
@@ -18,16 +23,83 @@ type Lock struct {
 //
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
+// assume lock state is ready or in use
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	lk := &Lock{ck: ck}
-	// You may add code here
+	lk.id = kvtest.RandValue(8)
+	lk.key = l
+	// randKey := l + kvtest.RandValue(8)
+	// lk.ck.Put(randKey, "ready", rpc.Tversion(0))
+	// lk.key = randKey
 	return lk
 }
 
+/*
+key -> value,version
+key is a constant, value is client's id and version is internally managed by the server
+once acquired, we reject any other put because the value is not theirs
+once released, we remove the client id from the value
+*/
 func (lk *Lock) Acquire() {
-	// Your code here
+	for {
+		value, version, getErr := lk.ck.Get(lk.key)
+		// lock free to be acquired
+		if getErr == rpc.OK && value == "" {
+			putErr := lk.ck.Put(lk.key, lk.id, version)
+			if putErr == rpc.ErrVersion {
+				continue
+			} else {
+				break
+			}
+		}
+		if getErr == rpc.OK && value != lk.id {
+			continue
+		}
+		if getErr == rpc.ErrNoKey || value == lk.id {
+			putErr := lk.ck.Put(lk.key, lk.id, version)
+			if putErr == rpc.ErrVersion {
+				continue
+			} else {
+				break
+			}
+		}
+	}
 }
 
 func (lk *Lock) Release() {
-	// Your code here
+	_, version, getErr := lk.ck.Get(lk.key)
+	if getErr == rpc.OK {
+		lk.ck.Put(lk.key, "", version)
+	} else {
+		fmt.Print("lock can't be released")
+	}
 }
+
+/*
+for {
+		value, version, err := lk.ck.Get(lk.key)
+		if err == rpc.ErrNoKey {
+			lk.ck.Put(lk.key, strconv.Itoa(0), rpc.Tversion(0))
+			return
+		}
+		v, _ := strconv.Atoi(value)
+		if v != int(version) {
+			continue
+		} else {
+			resErr2 := lk.ck.Put(lk.key, value, version)
+			if resErr2 != nil {
+
+			}
+			return
+		}
+	}
+*/
+
+/*
+lk.mu.Lock()
+	defer lk.mu.Unlock()
+	_, version, err := lk.ck.Get(lk.key)
+	if err == rpc.OK {
+		lk.ck.Put(lk.key, strconv.Itoa(int(version)+1), version)
+	}
+*/
